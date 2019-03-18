0e7b088939f9d01c31ffe8b4cc7244930464a93f
derrick rocha <derrickrocha@derricks-MacBook-Pro.local>
2019-03-18 11:05:19 -0600

    Start of MainActivity and MainFragment. Updated styles.

---
+++ app/src/main/java/com/cearleysoftware/byob/extensions/FragmentExtensions.kt
@@ -0,0 +1,2 @@
+package com.cearleysoftware.byob.extensions
+
--- app/src/main/java/com/cearleysoftware/byob/ui/activities/MainActivity.kt
+++ app/src/main/java/com/cearleysoftware/byob/ui/activities/MainActivity.kt
@@ -3,11 +3,24 @@ package com.cearleysoftware.byob.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cearleysoftware.byob.R
+import com.cearleysoftware.byob.databinding.MainActivityBinding
+import com.cearleysoftware.byob.extensions.replaceFragment
+import com.cearleysoftware.byob.extensions.setDataBindingContentView
+import com.cearleysoftware.byob.ui.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: MainActivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = setDataBindingContentView(R.layout.main_activity)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        replaceFragment(fragment = MainFragment())
+
     }
 }
