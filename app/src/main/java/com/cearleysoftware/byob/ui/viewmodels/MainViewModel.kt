1beba662792ccbea69a07083822aa0a42c328660
Derrick Rocha <drocha616@gmail.com>
2019-03-19 18:23:36 -0600

    CustomizeFragment. (#3)
    * CustomizeFragment.
    
    * added heart icon to toolbar.
    
    * Updated button strings.
    
    * Fixed button padding.

--- app/src/main/java/com/cearleysoftware/byob/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/cearleysoftware/byob/ui/viewmodels/MainViewModel.kt
@@ -1,14 +1,31 @@
 package com.cearleysoftware.byob.ui.viewmodels
 
+import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cearleysoftware.byob.constants.Constants
+import com.cearleysoftware.byob.models.Drink
+import com.cearleysoftware.byob.util.SingleLiveEvent
 
 class MainViewModel: ViewModel() {
 
+    private var currentFavoriteDrink: Drink? = null
+    private var customizableDrink: Drink? = null
+
+
     val navigateToViewDrinks: LiveData<Int> get() = _navigateToViewDrinks
     private val _navigateToViewDrinks = MutableLiveData<Int>()
+
+    val navigateToFavoriteDrink = SingleLiveEvent<Unit>()
+
+    val navigateToCoffeeBase = SingleLiveEvent<Unit>()
+
+    private val _hasFavoriteDrinkToSave = MutableLiveData<Boolean>()
+    val hasFavoriteDrinkToSave: LiveData<Boolean> get() = _hasFavoriteDrinkToSave
+
+    val hasCurrentDrink: Boolean
+        get() = currentFavoriteDrink != null
 
     fun hotDrinksClicked(){
         _navigateToViewDrinks.value = Constants.HOT_DRINKS
@@ -20,5 +37,19 @@ class MainViewModel: ViewModel() {
 
     fun teasClicked(){
         _navigateToViewDrinks.value = Constants.TEAS
+    }
+
+    fun currentDrinkClicked(){
+        if (currentFavoriteDrink != null) {
+            navigateToFavoriteDrink.call()
+        }
+    }
+
+    fun customizeDrinkClicked(){
+        if (currentFavoriteDrink != null){
+            currentFavoriteDrink = null
+            customizableDrink = null
+        }
+        navigateToCoffeeBase.call()
     }
 }
