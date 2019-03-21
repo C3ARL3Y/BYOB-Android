57cf8950d314584db38f3d63005f861eadf1294f
Derrick Rocha <drocha616@gmail.com>
2019-03-20 23:26:29 -0600

    Build alex fragment layout. (#4)
    * Build alex fragment layout.
    
    * Listener for login button.
    
    * Code cleanup.

--- app/src/main/java/com/cearleysoftware/byob/ui/fragments/alex/AlexFragment.kt
+++ app/src/main/java/com/cearleysoftware/byob/ui/fragments/alex/AlexFragment.kt
@@ -1,8 +1,31 @@
 package com.cearleysoftware.byob.ui.fragments.alex
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cearleysoftware.byob.R
+import com.cearleysoftware.byob.databinding.FragmentAlexBinding
+import com.cearleysoftware.byob.extensions.inflateWithBinding
+import com.cearleysoftware.byob.ui.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.fragment_alex.*
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 //  Copyright © 2019 Cearley Software. All rights reserved.
 
 class AlexFragment: Fragment() {
+
+    private lateinit var binding: FragmentAlexBinding
+    private val mainViewModel by sharedViewModel<MainViewModel>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = inflater.inflateWithBinding(R.layout.fragment_alex, container)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        login.setOnClickListener {  }
+    }
 }
