c80ea40531c54da5d0f276549acb27e0c5cf246a
derrick rocha <derrickrocha@derricks-MacBook-Pro.local>
2019-03-21 21:58:25 -0600

    Code cleanup.

--- app/src/main/java/com/cearleysoftware/byob/models/Drink.kt
+++ app/src/main/java/com/cearleysoftware/byob/models/Drink.kt
@@ -1,12 +1,10 @@
 package com.cearleysoftware.byob.models
 
 import android.os.Parcelable
-import com.google.firebase.database.IgnoreExtraProperties
 import kotlinx.android.parcel.Parcelize
 
 //  Copyright © 2019 Cearley Software. All rights reserved.
 
-@IgnoreExtraProperties
 @Parcelize
 data class Drink(var id: Int,
                  var name: String,
